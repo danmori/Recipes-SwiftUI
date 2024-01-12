708358c72441b2cd4f4003b0fe4ba5f0d1b1f41c
Daniel Mori <danielstelzer@gmail.com>
2024-01-12 15:06:13 -0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Dan Mori on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
